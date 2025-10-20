4c2afbaf8ebc131c165e9feec83a26a6f1393f11
Ujwal <ujwalcse01@gmail.com>
2025-10-20 21:10:41 +0530

    Pagination BE and FE both

---
+++ Pagination_BE_And_FE/Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2> &#x5350; &#21328; &#21328; &#21328; &#21328;</h2>
+      <h1>Ganpati Bappa Morya</h1>
+      <h2> &#x5350; &#21328; &#21328; &#21328; &#21328;</h2>
+    </div>
+  );
+}
+
+export default App;
